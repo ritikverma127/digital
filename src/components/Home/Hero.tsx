8f0bc6e6a5c217a1205abdb82bcaa2aff0d961ea
ritikverma127 <rv1338739@gmail.com>
2025-02-20 12:46:50 +0530

    set the hero page bottom side

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,25 +1,23 @@
 export default function Page() {
   return (
-    <div className="min-h-screen bg-white ">
-      <div className="container mx-auto px-4 py-4">
-        <main className="max-w-6xl mx-auto text-center space-y-14 sm:space-y-4 mb-12 mt-16">
-          <h1 className="text-4xl md:text-6xl font-bold font-sans text-newPrimary tracking-tight">
-            Get Traffic That Converts.
-          </h1>
-          <p className="text-lg text-muted-foreground max-w-2xl mx-auto">
-            Your Ad Campaigns Seen by Millions Daily on Premium Publisher Sites
-          </p>
-        </main>
+    <div className="container mx-auto px-4 py-4">
+      <main className="max-w-6xl mx-auto text-center space-y-14 mb-4 sm:space-y-4 mt-16">
+        <h1 className="text-4xl md:text-6xl font-bold font-sans text-newPrimary tracking-tight">
+          Get Traffic That Converts.
+        </h1>
+        <p className="text-lg text-muted-foreground max-w-2xl mx-auto">
+          Your Ad Campaigns Seen by Millions Daily on Premium Publisher Sites
+        </p>
+      </main>
 
-        <div className="relative max-w-4xl mx-auto space-y-14 sm:space-y-4">
-          <div className="relative rounded-2xl overflow-hidden">
-            <video src="/Desktop.webm" autoPlay loop muted></video>
-            <div className="absolute inset-0 bg-[#1a3c61]/60 flex items-center justify-center"></div>
-          </div>
-          <h2 className="text-2xl md:text-3xl font-bold text-[#1a3c61] text-center mt-6">
-            Run Ads on Premium Publisher Sites
-          </h2>
+      <div className="relative max-w-4xl mx-auto space-y-14 mb-4 sm:space-y-4">
+        <div className="relative rounded-2xl overflow-hidden">
+          <video src="/Desktop.webm" autoPlay loop muted></video>
+          <div className="absolute inset-0 bg-[#1a3c61]/60 flex items-center justify-center"></div>
         </div>
+        <h2 className="text-2xl md:text-3xl font-bold text-[#1a3c61] text-center mt-6">
+          Run Ads on Premium Publisher Sites
+        </h2>
       </div>
     </div>
   );
