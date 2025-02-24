910e1ddf926b7d92b1a19ea22bcaafaf7ca1f30c
ritikverma127 <rv1338739@gmail.com>
2025-02-24 13:19:02 +0530

    change frame

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -6,16 +6,16 @@ export default function Page() {
     <div className="container mx-auto px-4 py-4">
       <main className="max-w-6xl mx-auto text-center space-y-14 mb-4 sm:space-y-4 mt-16">
         <motion.h1
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, ease: "easeInOut" }}
           className="text-4xl md:text-4xl  font-bold font-sans text-newPrimary tracking-tight"
         >
           Accelerate Growth with Digital Innovation
         </motion.h1>
         <motion.p
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, ease: "easeInOut" }}
           className="text-xl text-muted-foreground max-w-2xl mx-auto"
         >
@@ -26,8 +26,8 @@ export default function Page() {
 
       <div className="relative max-w-4xl mx-auto space-y-14 mb-4 sm:space-y-4">
         <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, ease: "easeInOut" }}
           className="relative rounded-2xl overflow-hidden h-96 md:h-[32rem]"
         >
@@ -41,8 +41,8 @@ export default function Page() {
           {/* <div className="absolute inset-0 bg-[#1a3c61]/60 flex items-center justify-center"></div> */}
         </motion.div>
         <motion.h2
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, ease: "easeInOut" }}
           className="text-2xl md:text-3xl font-bold text-[#1a3c61] text-center mt-6"
         >
